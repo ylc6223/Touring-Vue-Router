0bd6c36d3f1b8f88375a81e789b7ee2b2959ce18
Jarry Yang <260902108@qq.com>
2022-07-28 14:43:31 +0800

    路由独享守卫

--- src/main.js
+++ src/main.js
@@ -1,13 +1,12 @@
-import { createApp, reactive } from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
+import GStore from './store'
 import 'nprogress/nprogress.css'
 
-const GStore = reactive({ flashMessage: '' })
+// const GStore = reactive({ flashMessage: '' })
 
 createApp(App)
-  .use(store)
   .use(router)
   .provide('GStore', GStore)
   .mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,8 @@ import About from '../views/About.vue'
 import NotFound from '@/views/NotFound.vue'
 import NetworkError from '@/views/NetworkError.vue'
 import NProgress from 'nprogress'
+import EventService from '@/services/EventService.js'
+import GStore from '@/store'
 const routes = [
   {
     path: '/',
@@ -18,8 +20,24 @@ const routes = [
   {
     path: '/events/:id',
     name: 'EventLayout',
-    props: true,
+    // props: true,
     component: EventLayout,
+    beforeEnter: to => {
+      return EventService.getEvent(to.params.id)
+        .then(response => {
+          GStore.event = response.data
+        })
+        .catch(error => {
+          if (error.response && error.response.status == 404) {
+            return {
+              name: '404Resource',
+              params: { resource: 'event' }
+            }
+          } else {
+            return { name: 'NetworkError' }
+          }
+        })
+    },
     children: [
       {
         path: '',
