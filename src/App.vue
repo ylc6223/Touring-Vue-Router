e69250b319caa115cf42a478a3c34754bc8d553a
Jarry Yang <260902108@qq.com>
2022-07-29 10:28:29 +0800

    路由滚动行为

--- src/App.vue
+++ src/App.vue
@@ -38,6 +38,7 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  height: 2000px;
 }
 
 #nav {
--- src/router/index.js
+++ src/router/index.js
@@ -88,7 +88,17 @@ const routes = [
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
+  routes,
+  // scrollBehavior() {
+  //   return { top: 0 }
+  // }
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { top: 0 }
+    }
+  }
 })
 /* eslint-disable */
 router.beforeEach((to,from) => {
